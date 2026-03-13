bd07b9401b615005c62599ed9d1235918d3983c3
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-03-13 16:12:15 -0300

    classes  Cliente, Pessoa Fisica e Conta

--- src/main.py
+++ src/main.py
@@ -2,28 +2,93 @@ from abc import ABC
 from datetime import datetime
 
 class Cliente:
-    pass
+    # caracteristicas
+    #contrutor inicializador:
+    def __init__(self, endereco):
+        self.__endereco = endereco
+        self.__contas = []
+
+    # atributos
+    def adicionar_conta(self, conta):
+        self.__contas.append(conta)
+        print("Conta adicionada ao cliente com sucesso!")
+
+    def listar_contas(self):
+        return self.__contas
+    
+# -----------------------------------
 
 class PessoaFisica(Cliente):
+    def __init__(self, endereco, cpf, nome):
+        super().__init__(endereco)
+        self.__cpf = cpf
+        self.__nome = nome
+        self.__data_nascimento = datetime
     pass
 
+# -----------------------------------
 
 class Conta:
-    pass
+    def __init__(self, numero, agencia, cliente, historico):
+        self.__saldo = 0.0
+        self.__numero = numero
+        self.__agencia = agencia
+        self.__cliente = cliente
+        self.__historico = []
 
-class ContaCorrente(Conta):
-    pass
+    
+    @property
+    def saldo(self):
+        return self.__saldo
+    
+    @classmethod
+    def nova_conta(cls, cliente, numero, agencia):
+        return cls(cliente, numero, agencia)
+
+    def sacar(self, valor):
+        if valor > 0 and valor <= self.__saldo:
+            self.__saldo -= valor
+            print(f"Saque de R${valor} realizado.")
+            return True
+        print("Saldo insuficiente ou valor inválido.")
+        return False
+    
+    def depositar(self, valor):
+        if valor > 0:
+            self.__saldo += valor
+            print(f"Depósito de R${valor} realizado.")
+            return True
+        return False
+    
+# -----------------------------------
     
 
+class ContaCorrente(Conta):
+    def __init__(self, saldo, numero, agencia, Cliente, Historico, limite, limite_saques):
+        super().__init__(saldo, numero, agencia, Cliente, Historico)
+        self.__limite = limite
+        self.__limite_saques = limite_saques
+    pass
+    
+# -----------------------------------
+
 class Historico:
+    def __init__(self, adiconar_transa= Transacao):
+        self.adiconar_transacao = adiconar_transa
     pass
 
+# -----------------------------------
 
 class Transacao(ABC):
+    
     pass
+
+# -----------------------------------
 
 class Saque(Transacao):
     pass
+
+# -----------------------------------
 
 class Deposito(Transacao):
     pass
