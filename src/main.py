a1e2d8bf9d8cf3628256a59c5b70959e568c0c02
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-03-13 13:36:23 -0300

    cria as classes

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
-from abc import ABC, abstractclassmethod, abstractproperty
-
+from abc import ABC
 from datetime import datetime
 
 class Cliente:
     pass
 
 class PessoaFisica(Cliente):
+    pass
+
+
+class Conta:
+    pass
+
+class ContaCorrente(Conta):
+    pass
+    
+
+class Historico:
+    pass
+
+
+class Transacao(ABC):
+    pass
+
+class Saque(Transacao):
+    pass
+
+class Deposito(Transacao):
     pass
