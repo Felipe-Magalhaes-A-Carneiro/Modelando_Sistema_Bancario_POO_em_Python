56a604b152ad4e53af94e7fea65b411fd3dc13ee
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2026-03-17 14:37:59 -0300

    update(main-v2): adapta codigo para a versao 2 do main

--- src/main-v2.py
+++ src/main-v2.py
@@ -3,9 +3,6 @@ from datetime import datetime
 
 # VERSÃO 2 = Utilizando INPUTS
 
-clientes = []
-contas = []
-
 class Cliente:
     # caracteristicas
     #contrutor inicializador:
@@ -16,7 +13,6 @@ class Cliente:
     # atributos
     def adicionar_conta(self, conta):
         self.__contas.append(conta)
-        print("\nConta adicionada ao cliente com sucesso!")
 
     def listar_contas(self):
         return self.__contas
@@ -46,6 +42,13 @@ class PessoaFisica(Cliente):
         data_nascimento = input("Informe a data de nascimento (dd-mm-aaaa)")
         endereco = input("Informe o endereço: ")
 
+        # Criamos o objeto usando as variáveis que acabamos de preencher
+        novo_cliente = PessoaFisica(nome=nome, data_nascimento=data_nascimento, cpf=cpf, endereco=endereco)
+        
+        # Guardamos na lista
+        clientes.append(novo_cliente)
+        print("\n=== Cliente criado com sucesso! ===")
+
 # -----------------------------------
 
 class Conta:
@@ -64,17 +67,15 @@ class Conta:
     @property
     def historico(self):
         return self.__historico
-    
-    @classmethod
-    def nova_conta(cls, cliente, numero, agencia):
-        return cls(cliente, numero, agencia)
 
     def sacar(self, valor):
-        if valor > 0 and valor <= self.__saldo:
+        if valor > self.__saldo:
+            print("\nSaldo insuficiente ou valor inválido.")
+            return False
+        if valor > 0:
             self.__saldo -= valor
             print(f"\nSaque de R${valor} realizado.")
             return True
-        print("\nSaldo insuficiente ou valor inválido.")
         return False
     
     def depositar(self, valor):
@@ -93,16 +94,10 @@ class ContaCorrente(Conta):
         self.limite = limite
         self.limite_saques = limite_saques
 
-        # Sobrescreve o método sacar para incluir regras de limite de cheque especial:
-
-        def sacar(self, valor):
-            # Limite e número de saques
-            return super().sacar(valor)
-    pass
-    
 # -----------------------------------
 
 class Historico:
+
     def __init__(self, arquivo = "historico.txt"):
         self.arquivo = arquivo
         self._transacoes = []
@@ -110,25 +105,10 @@ class Historico:
     @property
     def transacoes(self):
         return self._transacoes
-    
-    # Persistência de Dados em .txt
-    def _carregar_do_disco(self):
-        try:
-            with open(self.arquivo, "alteracao") as f:
-                return [linha.strip() for linha in f.readlines()]
-        except FileNotFoundError:
-            return []
-    
-    # def adicionar_transacao(self, transacao):
-    #     self._transacoes.append({
-    #         "tipo": transacao.__class__.__name__ ,
-    #         "valor": transacao.valor,
-    #         "data": datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-    #     })
 
     def adicionar_transacao(self, transacao):
-        data_hora = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
-        registro = f"{data_hora} - {transacao.__class__.__name__}: R${transacao.valor:.2f}"
+
+        registro = f"{datetime.now().strftime("%d-%m-%Y %H:%M:%S")} - {transacao.__class__.__name__}: R${transacao.valor:.2f}"
 
         self._transacoes.append(registro)
 
@@ -160,9 +140,8 @@ class Saque(Transacao):
         return self._valor
 
     def registrar(self, conta):
-        sucesso = conta.sacar(self.valor)
 
-        if sucesso:
+        if conta.sacar(self.valor):
             conta.historico.adicionar_transacao(self)
 
 # -----------------------------------
@@ -182,28 +161,4 @@ class Deposito(Transacao):
             conta.historico.adicionar_transacao(self)
 
 
-# --- EXECUÇÃO ---
-
-# 1. Criando o Cliente (Pessoa Física):
-novo_cliente = PessoaFisica(nome = nome, data_nascimento= data_nascimento,  cpf = cpf, endereco = endereco)
-
-# 2. Criando a Conta Corrente e vinculando ao cliente:
-conta = ContaCorrente(numero = 1, cliente =  novo_cliente )
-novo_cliente .adicionar_conta(conta)
-
-# 3. Ralizando Depósito:
-deposito = Deposito(1000.0)
-novo_cliente .realizar_transacao(conta, deposito)
-
-# 4. Realizando um Saque:
-saque = Saque(55.0)
-novo_cliente .realizar_transacao(conta, saque)
-
-# 5. Exibindo o resultado final:
-print(f"""
-Cliente: {novo_cliente.nome}
-
-Saldo Atual: R${conta.saldo:.2f}
-
----> Extrato em 'historico.txt' foi atualizado
-""")
+# ---------------- FUNÇÕES DO SISMTE MENU -------------------
